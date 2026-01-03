a3a94d120fea5618b008503bebb217c511974623
zzzzz <2659294465@qq.com>
2026-01-03 21:51:40 +0800

    add : 查询密钥

--- src/main/java/com/ityfz/yulu/chat/mapper/ChatSessionMapper.java
+++ src/main/java/com/ityfz/yulu/chat/mapper/ChatSessionMapper.java
@@ -6,4 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ChatSessionMapper extends BaseMapper<ChatSession> {
+
+    // 这里可以定义一些自定义的SQL方法，例如查询会话详情、更新会话状态等。
+    // 例如：
+    ChatSession selectByPrimaryKey(Long id);
 }
